f5434d9bf5514f0f16b747c6450a4a65c1fb3622
Andres Soerensen <andressoerensen@gmail>
2020-06-29 18:27:16 -0300

    modific sw.js

--- sw.js
+++ sw.js
@@ -2,10 +2,7 @@
 //asignar un nombre y versión al cache
 const CACHE_NAME = 'v1_cache_personal_site',
   urlsToCache = [
-    './',
-    './about.html',
-    './services.html' 
-              
+    './index.html'              
   ];
 
 //durante la fase de instalación, generalmente se almacena en caché los activos estáticos
