79d767eb80f720598ac0479c2f439be966755ceb
Andres Soerensen <andressoerensen@gmail>
2020-06-30 10:01:53 -0300

    add fuction request permision notification

--- registerSW.js
+++ registerSW.js
@@ -69,4 +69,22 @@ window.onload = (e) => {
 		});
 	});
 
+	//Request user permission to notification
+	function askPermission() {
+		return new Promise(function(resolve, reject) {
+		  const permissionResult = Notification.requestPermission(function(result) {
+			resolve(result);
+		  });
+	  
+		  if (permissionResult) {
+			permissionResult.then(resolve, reject);
+		  }
+		})
+		.then(function(permissionResult) {
+		  if (permissionResult !== 'granted') {
+			throw new Error('We weren\'t granted permission.');
+		  }
+		});
+	  }
+
 }
