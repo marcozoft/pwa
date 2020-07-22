e90b751ea37a7959170a6789342aa4fa5d5fb0d7
Andres Soerensen <andressoerensen@gmail>
2020-07-22 09:25:43 -0300

    suscripcion a api

---
+++ pwaFiles/registerSW.js
@@ -0,0 +1,200 @@
+const publickey = 'BFzWsQGVxUb3GADJj2C5XNa2hoqPZWKVaz3TptLePhYcOaDqBmIDg7sKP-BV9aJiTnI9MN5y_4jatNgbxOV6jfM';
+const urlToSendSubscriptionOnServer= "http://api.victorhugovallejos.com.ar/";
+
+function urlB64ToUint8Array(base64String) {
+	const padding = '='.repeat((4 - base64String.length % 4) % 4);
+	const base64 = (base64String + padding)
+	  .replace(/\-/g, '+')
+	  .replace(/_/g, '/');
+  
+	const rawData = window.atob(base64);
+	const outputArray = new Uint8Array(rawData.length);
+  
+	for (let i = 0; i < rawData.length; ++i) {
+	  outputArray[i] = rawData.charCodeAt(i);
+	}
+	return outputArray;
+}
+
+function subscribeUser() {
+	const applicationServerKey = urlB64ToUint8Array(publickey);
+	swRegistration.pushManager.subscribe({
+	  userVisibleOnly: true,
+	  applicationServerKey: applicationServerKey
+	})
+	.then(function(subscription) {
+	  console.log('User is subscribed.');
+	  console.log('subscription for updateSubscriptionsONServer',JSON.stringify(subscription));
+	  updateSubscriptionOnServer(subscription);
+	})
+	.catch(function(err) {
+	  console.log('Failed to subscribe the user: ', err);
+	  
+	});
+  }
+
+  function updateSubscriptionOnServer(subscription) {
+	// TODO: Send subscription to application server
+	if (subscription) {	  
+  
+	  fetch(urlToSendSubscriptionOnServer, {
+			method: 'post',
+			mode: 'cors',
+            headers: new Headers({
+            'Content-Type': 'application/json'
+            }),
+			body: JSON.stringify(subscription)
+        }).then(function(response) {
+			console.log("response server: ",response)
+            if (!response.ok) {
+              throw new Error('Bad status code from server.');
+            }
+        
+            return response.json();
+          })
+          .then(function(responseData) {
+            if (!(responseData.data && responseData.data.success)) {
+              throw new Error('Bad response from server.');
+            }
+		  });
+		
+		  
+
+
+
+
+		  fetch(urlToSendSubscriptionOnServer, {
+			method: 'POST',
+        	headers: {
+            	'Content-Type': 'application/x-www-form-urlencoded'
+        	},
+        	body: 'a=1&b=2'
+        }).then(function(response) {
+			console.log("response server: ",response)
+            if (!response.ok) {
+              throw new Error('Bad status code from server.');
+            }
+        
+            return response.json();
+          })
+          .then(function(responseData) {
+            if (!(responseData.data && responseData.data.success)) {
+              throw new Error('Bad response from server.');
+            }
+          });  
+			  
+	} 
+  }
+
+//Request user permission to notification
+function askPermission() {
+	return new Promise(function(resolve, reject) {
+		
+	  const permissionResult = Notification.requestPermission(function(result) {
+		resolve(result);
+	  });
+  
+	  if (permissionResult) {
+		permissionResult.then(resolve, reject);
+	  }
+	})
+	.then(function(permissionResult) {
+	  if (permissionResult !== 'granted') {
+		throw new Error('We weren\'t granted permission.');
+	  }else{
+		  if(permissionResult === 'granted'){
+			subscribeUser();
+		  }
+	  }
+	});
+  }
+
+  window.onload = (e) => { 
+
+	if ('serviceWorker' in navigator && 'PushManager' in window) {
+		console.log('Service Worker and Push is supported');
+		navigator.serviceWorker.register('./pwaFiles/service-worker.js')
+			.then(registration =>{	
+				console.log('Service Worker is registered', registration);
+				swRegistration = registration;			
+				askPermission();		
+			})
+			.catch(function(error) {
+				console.error('Service Worker Error', error);
+			  });
+	}		
+	else{
+		console.warn('Push messaging is not supported');		
+	} 
+	
+	const prompt = document.querySelector('#prompt');
+	const promptNotification = document.querySelector('#promptNotification');
+	const buttonAdd = document.querySelector('#buttonAdd');
+	const buttonCancel = document.querySelector('#buttonCancel');
+	const buttonAccept = document.querySelector('#buttonAccept');
+	const buttonDenied = document.querySelector('#buttonDenied');
+
+	let deferredPrompt;
+	window.addEventListener('beforeinstallprompt', (e) => {
+		// Prevent Chrome 67 and earlier from automatically showing the prompt
+		e.preventDefault();
+		// Stash the event so it can be triggered later.
+		deferredPrompt = e;
+		// Show prompt modal if user previously not set to dismissed or accepted
+		if(!statusPrompt.get()) {
+		// Change status prompt
+		promptToggle(prompt, 'show', 'hide');
+		}
+		if(!statusPrompt.get()) {
+			// Change status prompt
+			promptToggle(promptNotification, 'show', 'hide');
+		}
+
+	});
+
+	// Add event click function for Cancel button
+	buttonAccept.addEventListener('click', (e) => {
+		// Change status prompt
+		promptToggle(promptNotification, 'hide', 'show');
+		// Set status prompt to dismissed
+		statusPrompt.set('dismissed');
+	});
+
+	// Add event click function for Cancel button
+	buttonDenied.addEventListener('click', (e) => {
+		// Change status prompt
+		promptToggle(promptNotification, 'hide', 'show');
+		// Set status prompt to dismissed
+		statusPrompt.set('dismissed');
+	});
+
+
+	// Add event click function for Cancel button
+	buttonCancel.addEventListener('click', (e) => {
+		// Change status prompt
+		promptToggle(prompt, 'hide', 'show');
+		// Set status prompt to dismissed
+		statusPrompt.set('dismissed');
+	});
+
+	// Add event click function for Add button
+	buttonAdd.addEventListener('click', (e) => {
+		// Change status prompt
+		promptToggle(prompt, 'hide', 'show');
+		// Show the prompt
+		deferredPrompt.prompt();
+		// Wait for the user to respond to the prompt
+		deferredPrompt.userChoice
+		.then((choiceResult) => {
+			if (choiceResult.outcome === 'accepted') {
+			statusPrompt.set('accepted');
+			console.log('User accepted the A2HS prompt');
+			} else {
+			statusPrompt.set('dismissed');
+			console.log('User dismissed the A2HS prompt');
+			}
+			deferredPrompt = null;
+		});
+	});
+
+}
---
+++ pwaFiles/service-worker.js
@@ -0,0 +1,86 @@
+
+//asignar un nombre y versión al cache
+const CACHE_NAME = 'v1_cache',
+  urlsToCache = [
+    
+  ];
+
+//durante la fase de instalación, generalmente se almacena en caché los activos estáticos
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache)
+          .then(() => self.skipWaiting())
+      })
+      .catch(err => console.log('Falló registro de cache', err))
+  )
+})
+
+//una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
+self.addEventListener('activate', e => {
+  const cacheWhitelist = [CACHE_NAME]
+
+  e.waitUntil(
+    caches.keys()
+      .then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            //Eliminamos lo que ya no se necesita en cache
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName)
+            }
+          })
+        )
+      })
+      // Le indica al SW activar el cache actual
+      .then(() => self.clients.claim())
+  )
+})
+
+//cuando el navegador recupera una url
+self.addEventListener('fetch', e => {
+  //Responder ya sea con el objeto en caché o continuar y buscar la url real
+  e.respondWith(
+    caches.match(e.request)
+      .then(res => {
+        if (res) {
+          //recuperar del cache
+          return res
+        }
+        //recuperar de la petición a la url
+        return fetch(e.request)
+      }).catch(() => {
+        if(e.request.url.indexOf('.html') > -1){
+          return caches.match('fallback.html');
+        }
+      })
+  )
+})
+
+
+self.addEventListener('push', function(event) {
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: `);
+  console.log(JSON.parse(event.data.text()));//modified from tutorial to make it more dynamic
+  const notificationObject = JSON.parse(event.data.text());//modified from tutorial to make it more dynamic
+
+  const title = notificationObject.title;//modified from tutorial to make it more dynamic
+  const options = {
+    body: notificationObject.msg,
+    icon: notificationObject.icon,
+    badge: notificationObject.badge
+  };
+  self.notificationURL = notificationObject.url;//modified from tutorial to make it more dynamic
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click Received.');
+  //console.log(self.notificationURL);
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow(self.notificationURL)//modified from tutorial to make it more dynamic
+  );
+});
